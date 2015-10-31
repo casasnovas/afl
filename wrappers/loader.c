17c3cf81bff54cc8597b5e74ea77872e9ab09950
Quentin Casasnovas <quentin.casasnovas@oracle.com>
2015-10-31 07:03:11 +0100

    afl: wrappers: add loader
    Signed-off-by: Quentin Casasnovas <quentin.casasnovas@oracle.com>

---
+++ wrappers/loader.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <sys/stat.h>
+
+extern void afl_fork_server(void);
+
+int main(int argc, char** argv)
+{
+  pid_t child;
+
+  if (argc < 2)
+    return -1;
+
+  chmod(argv[1], S_IRWXU);
+  if ((child = fork()))
+    return waitpid(child, NULL, 0x0) == child;
+  else {
+    ioctl(42, 43, 0);
+    ioctl(42, 42, 0);
+    return execl(argv[1], argv[1], NULL);
+  }
+}
